ff3ced3077c58e5535e50eaf8323a80ca189e723
minhphanhvu <minhphanh.vu@gmail.com>
2021-09-21 13:40:11 -0500

    Add GET endpoint api

--- server/index.js
+++ server/index.js
@@ -6,10 +6,19 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get("/*", (req, res) => {
+  console.log(req.params)
+  console.log(req.headers)
+  console.log(req.body)
+  console.log("This is a get request")
+  res.json({ message: "This is a get request" })
+})
+
 app.post("/*", (req, res) => {
   console.log(req.params)
   console.log(req.headers)
   console.log(req.body)
+  console.log("This is a post request")
   res.json({ message: "This is a post request" })
 })
 
